81c2168ca1f900f2003c04a7094ee696092d0c04
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-09-23 15:51:42 +0530

    init

---
+++ programs/stablecoin-q3-2026/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("GyS3h3kXEHADYhzHXR2r9VBMYejFNeGwg7Ln66v2bqo5");
+
+#[program]
+pub mod stablecoin_q3_2026 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
